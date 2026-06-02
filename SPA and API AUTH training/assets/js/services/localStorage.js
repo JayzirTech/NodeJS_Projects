1b85535f8b380452bc15176f878bc4798c7ce036
JayzirTech <jayzirslaider18@gmail.com>
2026-06-02 09:23:21 -0500

    Proyecto terminado

---
+++ SPA and API AUTH training/assets/js/services/localStorage.js
@@ -0,0 +1,21 @@
+export function saveLocalStorage(apiData) {
+    localStorage.setItem('apiData', JSON.stringify(apiData));
+}
+
+export function loadLocalStorage() {
+    if (localStorage.getItem('apiData')) {
+        return JSON.parse(localStorage.getItem('apiData'));
+    }
+    return [];
+}
+
+export function saveCharactersDeletedLocalStorage(charactersDeleted) {
+    localStorage.setItem('charactersDeleted', JSON.stringify(charactersDeleted));
+}
+
+export function loadCharactersDeletedLocalStorage() {
+    if (localStorage.getItem('charactersDeleted')) {
+        return JSON.parse(localStorage.getItem('charactersDeleted'));
+    }
+    return [];
+}
